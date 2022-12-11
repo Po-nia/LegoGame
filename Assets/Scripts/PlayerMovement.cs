54355f6366e99b2570af7d1023dc251dc8e8b107
KamilaKisiel <kisiel.kamila@gmail.com>
2022-12-11 20:21:47 +0100

    L83

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,11 +9,12 @@ public class PlayerMovement : MonoBehaviour
 
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
+    Animator myAnimator;
     
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
-        
+        myAnimator = GetComponent<Animator>();        
     }
 
     void Update()
@@ -32,6 +33,9 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
     }
     void FlipSprite()
     {
