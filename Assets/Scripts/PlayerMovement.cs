d3c55d2fc2def4b47d7144da5b06f0b62ed24272
KamilaKisiel <kisiel.kamila@gmail.com>
2022-12-14 21:32:24 +0100

    L88

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -68,12 +68,15 @@ public class PlayerMovement : MonoBehaviour
         if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) 
         {
             myRigidbody.gravityScale = gravityScaleAtStart;
+            myAnimator.SetBool(("isClimbing"), false);
             return;
         }
-        
         
         Vector2 climbVelocity = new Vector2 (myRigidbody.velocity.x, moveInput.y * climbSpeed);
         myRigidbody.velocity = climbVelocity; 
         myRigidbody.gravityScale = 0f; 
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool(("isClimbing"), playerHasVerticalSpeed); 
     }
 }
