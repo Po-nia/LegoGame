03eaa2c6718376938ea41deac6113ae09837d2b8
KamilaKisiel <kisiel.kamila@gmail.com>
2022-12-20 17:44:39 +0100

    L99

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float jumSpeed = 5f;
     [SerializeField] float climbSpeed = 5f;
     [SerializeField] Vector2 deathKick = new Vector2(10f, 10f);
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform gun;
 
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
@@ -36,6 +38,13 @@ public class PlayerMovement : MonoBehaviour
         ClimbLadder();
         Die();
         
+    }
+
+    void OnFire(InputValue value)
+    {
+        if (!isAlive) { return; }
+        Instantiate(bullet, gun.position, transform.rotation);
+
     }
 
     void OnMove(InputValue value)
