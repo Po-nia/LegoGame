99deea64cb3c665c6a870f0f75db4baad54248a3
KamilaKisiel <kisiel.kamila@gmail.com>
2022-12-27 15:16:30 +0100

    L105
    coin sound effect

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+   [SerializeField] AudioClip coinPickupSFX;
+   
    void OnTriggerEnter2D(Collider2D other)
    {
     if (other.tag == "Player")
     {
+        AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
    }
