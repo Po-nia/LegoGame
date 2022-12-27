cea7c42d1ed0e8baee0fe85f1720a905b41025d8
KamilaKisiel <kisiel.kamila@gmail.com>
2022-12-27 16:10:21 +0100

    L106
    score & lives

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
    [SerializeField] AudioClip coinPickupSFX;
+   [SerializeField] int pointsForCoinPickup = 100;
+
+   bool wasCollected = false;
    
    void OnTriggerEnter2D(Collider2D other)
    {
-    if (other.tag == "Player")
+    if (other.tag == "Player" && !wasCollected)
     {
+        wasCollected = true;
+        FindObjectOfType<GameSession>().AddToScore(pointsForCoinPickup);
         AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
+        gameObject.SetActive(false);
         Destroy(gameObject);
     }
    }
--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int playerLives = 3;
+    [SerializeField] int score = 0;
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI scoreText;
     
     void Awake()
     {
@@ -21,6 +25,13 @@ public class GameSession : MonoBehaviour
             }
         }    
     }
+
+    void Start()
+    {
+        livesText.text = playerLives.ToString();
+        scoreText.text = score.ToString();
+    }
+
     public void ProcessPlayerDeath()
     {
         if (playerLives > 1)
@@ -33,11 +44,19 @@ public class GameSession : MonoBehaviour
         }
     }
 
+    public void AddToScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        scoreText.text = score.ToString();
+    }
+
     void TakeLife()
     {
         playerLives --;
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+        livesText.text = playerLives.ToString();
+        
     }
 
     void ResetGameSession()
